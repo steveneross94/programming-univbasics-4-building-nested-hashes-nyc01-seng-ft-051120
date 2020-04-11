da46e470ccd1f5d71a543e9fa5d2e2929ee23090
Steven Ross <ross.steven@me.com>
2020-04-11 14:46:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,8 +11,8 @@ def monopoly_with_second_tier
   # complexity.
   base_hash = {
     railroads: {
-      pieces: 4
-      rent_in_dollars: {}
+      pieces: 4,
+      rent_in_dollars: {},
       names: {}
             }
           }
